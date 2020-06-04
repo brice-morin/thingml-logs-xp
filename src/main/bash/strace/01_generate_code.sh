45f51c68b4a9a7e65cc0ae70cdca21d20d30d46f
brice-morin <brice.morin1@gmail.com>
2020-06-05 00:23:44 +0200

    wip simulation on syscalls

--- src/main/bash/strace/01_generate_code.sh
+++ src/main/bash/strace/01_generate_code.sh
@@ -6,14 +6,13 @@ mkdir -p $PLATFORMDIR
 rm -r $PLATFORMDIR/* 2> /dev/null
 
 ### Generate platform code ###
-logo
 for LANGUAGE in ${LANGUAGES[@]}; do
   mkdir -p $PLATFORMDIR/$LANGUAGE
   echo "-- GENERATING CODE FOR $LANGUAGE --"
-  
+
   COMPILER=`echo $LANGUAGE | cut -d'_' -f1`
-  
-  _docker run --rm -v $BASEDIR:/thingml-div thingml-div thingml -c $COMPILER -s /thingml-div/src/main/resources/experiments1/perf/$LANGUAGE.thingml -o /thingml-div/target/code/$LANGUAGE  
+
+  _docker run --rm -v $BASEDIR:/thingml-div thingml-div thingml -c $COMPILER -s /thingml-div/src/main/thingml/$LANGUAGE.thingml -o /thingml-div/target/code/$LANGUAGE
   _docker run --rm -v $BASEDIR:/thingml-div --entrypoint /bin/sh thingml-div -c "cd /thingml-div/target/code/$LANGUAGE/base && cloc . > cloc.log"
 done
 wait
